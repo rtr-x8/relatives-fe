d2196903bd33c9991d100bfdc934733ac5919800
rtr-x8 <ryu.2.24@hotmail.co.jp>
2019-06-29 18:23:15 +0900

    Fix App.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import AssertionsIndex from './components/assertions_index';
+
+export default class App extends React.Component {
+  render(h) {
+    return (
+      <AssertionsIndex />
+    );
+  }
+}
