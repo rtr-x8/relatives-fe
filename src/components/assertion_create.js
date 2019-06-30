3f09e5c9f6be6fda82ba0b1963a239e853f134b3
rtr-x8 <ryu.2.24@hotmail.co.jp>
2019-06-30 16:46:06 +0900

    Add bar for create page

--- src/components/assertion_create.js
+++ src/components/assertion_create.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import TextField from '@material-ui/core/TextField';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import KeyboardBackspace from '@material-ui/icons/KeyboardBackspace';
 
 export default class AssertionCreate extends React.Component {
   constructor(props) {
@@ -11,19 +16,39 @@ export default class AssertionCreate extends React.Component {
       validMessages: {},
     }
   }
+
+  async back() {
+    console.log("test")
+  }
+
   render(h) {
     return (
-      <form noValidate autoComplete="off" onSubmit={() => this.onSubmit}>
-        <TextField
-          id="outlined-dense-multiline"
-          label="Body"
-          margin="dense"
-          variant="outlined"
-          multiline
-          rowsMax="4"
-          fullWidth={true}
-        />
-      </form>
+      <React.Fragment>
+        <form noValidate autoComplete="off" onSubmit={() => this.onSubmit}>
+          <TextField
+            id="outlined-dense-multiline"
+            label="Body"
+            margin="dense"
+            variant="outlined"
+            multiline
+            rowsMax="4"
+            fullWidth={true}
+          />
+        </form>
+        <AppBar style={{bottom: 0, top: "auto"}}>
+          <Toolbar>
+            <IconButton edge="start" color="inherit" aria-label="back and save" onClick={this.back}>
+              <KeyboardBackspace />
+            </IconButton>
+            <div style={{
+              flexGrow: 1,
+            }} />
+            <IconButton edge="end" color="inherit" aria-label="Open drawer">
+              <MenuIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </React.Fragment>
     )
   }
 }
