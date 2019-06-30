c1f9e2dbd3c133a5518cd3dda23845652547d214
rtr-x8 <ryu.2.24@hotmail.co.jp>
2019-06-30 22:11:30 +0900

    Implement basic test , and fix test

--- src/components/assertion_create.test.js
+++ src/components/assertion_create.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AssertionsIndex from './assertions_index';
+import AssertionCreate from './assertion_create';
 import { BrowserRouter as Router } from "react-router-dom";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
     <Router>
-      <AssertionsIndex />
+      <AssertionCreate />
     </Router>
-  , div);
+    , div);
   ReactDOM.unmountComponentAtNode(div);
 });
